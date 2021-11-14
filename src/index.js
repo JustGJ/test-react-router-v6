2927d880686a9bf9039aee84c293f72df70e8c62
Jeff <jf.gasp@gmail.com>
2021-11-14 11:24:34 +0100

    Route encapasuled

--- src/index.js
+++ src/index.js
@@ -18,11 +18,11 @@ const Learn = () => {
             <h1>Learn</h1>
             <p>All courses are listed here</p>
             {/* Sur /learn, les deux buttons apparaitons. En cliquant sur courses par exemple, on sera mené vers /learn/course  */}
-            <Link to="/learn/course" className="btn btn-success">
+            <Link to="/learn/courses" className="btn btn-success">
                 courses
             </Link>
-            <Link to="/learn/bundle" className="btn btn-primary">
-                Bundle
+            <Link to="/learn/bundles" className="btn btn-primary">
+                Bundles
             </Link>
         </div>
     );
@@ -53,7 +53,11 @@ ReactDOM.render(
             <Route path="/" element={<Home />} />{' '}
             {/* sur /myapps, on redirige sur learn (aucun retour arrière possible). Replace permet le retour en arrière */}
             <Route path="/myapps" element={<Navigate replace to="/learn" />} />{' '}
-            <Route path="/learn" element={<Learn />} />
+            {/* /learn = route parent  /learn/courses & learn/bundles = routes enfants  l */}
+            <Route path="/learn" element={<Learn />}>
+                <Route path="courses" element={<Courses />} />
+                <Route path="bundles" element={<Bundle />} />
+            </Route>
         </Routes>
     </Router>,
     document.getElementById('root')
